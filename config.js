8f10f2f577737c9e0df44269aad3f5add1fc44fb
damstrivandrum <aajalms@gmail.com>
2026-08-01 23:38:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ window.MARKS_WRONG = -1;
 window.EXAM_TZ = "Asia/Kolkata";
 
 // ✅ Fixed exam window in IST
-window.EXAM_START_IST = "2026-08-01T10:00:00+05:30";
+window.EXAM_START_IST = "2026-08-02T10:00:00+05:30";
 window.EXAM_END_IST   = "2026-08-02T22:00:00+05:30";
 
 // ✅ Parsed milliseconds
